2df83cd75cf2a724db8cb659c39f72f41b1629f2
arsaizdihar <arsadihar@gmail.com>
2022-10-21 21:38:46 +0700

    Initialize ADT Makanan

---
+++ makanan/makanan.h
@@ -0,0 +1,13 @@
+/* File: makanan.h */
+/* *** Definisi ABSTRACT DATA TYPE MAKANAN *** */
+
+#ifndef MAKANAN_H
+#define MAKANAN_H
+
+typedef struct
+{
+  int id;
+  char *name;
+} Makanan;
+
+#endif
