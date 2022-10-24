a19d72cc3f50fbffb7069ed816e96dd86339570c
Mifkiyan <m.favian199@gmail.com>
2022-10-24 13:59:41 +0700

    fix stack to dynamic & tweak queue

--- Stack/stack.h
+++ Stack/stack.h
@@ -2,28 +2,36 @@
 #define STACK_H
 
 #include "../boolean.h"
+#include "../simulator/simulator.h"
 
 #define IDX_UNDEF -1
-#define CAPACITY 100
 
-typedef int ElType; // Masih perlu diubah
+typedef Sim ElType;
 typedef struct
 {
-    ElType buffer[CAPACITY]; /* tabel penyimpan elemen */
-    int Top;                 /* alamat TOP: elemen puncak */
+    ElType *buffer; /* tabel penyimpan elemen */
+    int Top;        /* alamat TOP: elemen puncak */
+    int maxStack;   /* Max elemen stack */
 } Stack;
 
 /* ********* AKSES (Selektor) ********* */
 /* Jika S adalah Stack, maka akses elemen : */
 #define IDX_TOP(S) (S).Top
 #define InfoTop(S) (S).buffer[(S).Top]
+#define MaxStack(S) (S).maxStack
 
-/* *** Konstruktor/Kreator *** */
-void CreateStack(Stack *S);
+/* *** Konstruktor *** */
+void CreateStack(Stack *S, int max);
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S kosong dengan kondisi sbb: */
 /* - Index top bernilai IDX_UNDEF */
 /* Proses : Melakukan alokasi, membuat sebuah S kosong */
+
+/* *** Destruktor *** */
+void dealocateStack(Stack *S);
+/* Proses: Mengembalikan memori S */
+/* I.S. S pernah dialokasi */
+/* F.S. S menjadi tidak terdefinisi lagi, MaxStack diset 0, IDX_TOP jadi IDX_UNDEF */
 
 /* ************ Prototype ************ */
 boolean isStackEmpty(Stack S);
@@ -42,5 +50,16 @@ void pop(Stack *S, ElType *state);
 /* Menghapus val dari Stack s */
 /* I.S. s tidak mungkin kosong */
 /* F.S. val adalah nilai elemen InfoTop yang lama, IDX_TOP berkurang 1 */
+
+/* ************ Mengubah ukuran Stack ************ */
+void expandStack(Stack *S, int num);
+/* Proses : Menambahkan MaxStack S sebanyak num */
+/* I.S. Stack sudah terdefinisi */
+/* F.S. Ukuran list bertambah sebanyak num */
+
+void shrinkStack(Stack *S, int num);
+/* Proses : Mengurangi MaxStack S sebanyak num */
+/* I.S. Stack sudah terdefinisi, ukuran MaxStack > num */
+/* F.S. Ukuran Stack berkurang sebanyak num. */
 
 #endif
