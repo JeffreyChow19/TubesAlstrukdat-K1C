5abb986d3619f962b64ec3306644cb1990a45728
HoseaNA <13521057@std.stei.itb.ac.id>
2022-10-26 20:12:00 +0700

    tweak simulator

--- simulator/simulator.c
+++ simulator/simulator.c
@@ -5,7 +5,17 @@ void CreateSim(Sim *S, String Name, Point Pos)
 {
     Name(*S) = Name;
     Pos(*S) = Pos;
+    CreateTime(&Clock(*S), 0, 0, 0);
     CreateQueue(&Inv(*S), 50);
+    CreateQueue(&Delv(*S), 50);
+}
+
+void copySim(Sim S, Sim *CS)
+{
+    Name(*CS) = Name(S);
+    Pos(*CS) = Pos(S);
+    Clock(*CS) = Clock(S);
+    /* masih perlu copy queue */
 }
 
 void moveSim(Sim *S, String dir)
@@ -20,10 +30,26 @@ void moveSim(Sim *S, String dir)
         nextPoint(&Pos(*S), 0, 1);
 }
 
+void addTime(Sim *S, int dd, int hh, int mm)
+{
+    Time add; 
+    CreateTime(&add, dd, hh, mm);
+    int added = TimeToMinute(add);
+    incNTime(&Clock(*S), added);
+}
+
+void removeTime(Sim *S, int dd, int hh, int mm)
+{
+    Time remove;
+    CreateTime(&remove, dd, hh, mm);
+    int removed = TimeToMinute(remove);
+    decNDuration(&Clock(*S), removed);
+}
+
 void getFood(Sim S, Makanan *val, int id)
 {
     int i = indexOf(Inv(S), id);
-    *val = (Food(S, i));
+    *val = (FoodInv(S, i));
 }
 
 void addFood(Sim *S, Makanan val)
@@ -56,7 +82,7 @@ void openInv(Sim S)
         while (i >= 0)
         {
             printf("%d. ", i + 1);
-            printWithExpired(Food(S, i));
+            printWithExpired(FoodInv(S, i));
             i++;
         }
     }
