7279a7434f868ede3d29b9e4eedb7253a499daab
Mifkiyan <m.favian199@gmail.com>
2022-10-29 11:44:57 +0700

    update prioQ & define undoredo

--- undo-redo/undoredo.h
+++ undo-redo/undoredo.h
@@ -1,5 +1,9 @@
+#ifndef UNDOREDO_H
+#define UNDOREDO_H
 #include "../adt/stack/stack.h"
 
 void undo(Sim *sim, Stack *utama, Stack *penampung);
 
 void redo(Sim *sim, Stack *utama, Stack *penampung);
+
+#endif
