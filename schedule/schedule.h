87f220972d52197e1e60e557826b8204a9f746db
Mifkiyan <m.favian199@gmail.com>
2022-11-01 03:44:59 +0700

    add description to functions-schedule

--- schedule/schedule.h
+++ schedule/schedule.h
@@ -1,8 +1,12 @@
 #ifndef SCHEDULE_H
 #define SCHEDULE_H
 
+/* Menambah satu menit waktu berjalannya simulator; */
+/* Mengurangi satu menit waktu expiry, pengolahan, dan delivery makanan; */
 void tick();
 
+/* Menambah waktu berjalannya simulator sebanyak h jam dan m menit; */
+/* Mengurangi waktu expiry, pengolahan, dan delivery makanan sebanyak h jam dan m menit; */
 void tickWithTime(int h, int m);
 
 #endif
