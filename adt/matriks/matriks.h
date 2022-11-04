118c002d6c18d69b27785d3293c630c2e4bdeb5a
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-04 22:25:19 +0700

    driver char,word machine and matriks

--- adt/matriks/matriks.h
+++ adt/matriks/matriks.h
@@ -26,32 +26,34 @@ typedef struct
 #define ELMTMat(M, i, j) (M).mem[(i)][(j)]
 
 /* Constructor */
+/* Inisialisasi matrix */
 void createMatrix(int nRows, int nCols, Matrix *m);
 
-boolean isMatrixIdxValid(int i, int j);
 /*Mengirimkan true jika i, j adalah index yang valid untuk matriks apapun*/
+boolean isMatrixIdxValid(int i, int j);
 
-IdxType getLastIdxRow(Matrix m);
 /* Mengirimkan Index baris terbesar m */
-IdxType getLastIdxCol(Matrix m);
+IdxType getLastIdxRow(Matrix m);
 /* Mengirimkan Index kolom terbesar m */
-boolean isIdxEffMat(Matrix m, IdxType i, IdxType j);
+IdxType getLastIdxCol(Matrix m);
 /* Mengirimkan true jika i, j adalah Index efektif bagi m */
+boolean isIdxEffMat(Matrix m, IdxType i, IdxType j);
 
-void copyMatrix(Matrix mIn, Matrix *mOut);
 /* Melakukan assignment mOut <- mIn */
+void copyMatrix(Matrix mIn, Matrix *mOut);
 
-void readMatrix(Matrix *m, char *filename, Point *simPoint);
 /* I.S. filename valid, m sembarang*/
 /* F.S. m terdefinisi nilai elemen efektifnya, berukuran nRow x nCol, nRow dan nCol dicari dari file */
 /* Proses: Melakukan CreateMatrix(m,nRow,nCol) dan mengisi nilai efektifnya */
 /* Selanjutnya membaca nilai elemen per baris dan kolom dari file:filename dan memindahkannya ke matriks*/
+void readMatrix(Matrix *m, char *filename, Point *simPoint);
 
-void displayMatrix(Matrix m, Point simPoint);
 /* I.S. m dan s terdefinisi */
 /* F.S. Nilai m(i,j) ditulis ke layar per baris per kolom, matriks diberi border berupa '*' dan jika membaca # dari file akan diganti dengan ' '*/
 /* Proses: Menulis nilai setiap elemen m ke layar dengan traversal per baris dan per kolom */
+void displayMatrix(Matrix m, Point simPoint);
 
+/* mengirimkan true jika adjacent dari simPoint adalah action, vice versa.*/
 boolean isActionAdj(Matrix m, Point simPoint, char action);
 
 #endif
